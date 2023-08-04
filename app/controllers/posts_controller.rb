b866f75bc28766bcbe97ed8e665a6c8b2d9ad627
Kd-20028 <keshawndavis472@gmail.com>
2023-08-03 22:42:55 -0500

    fix: Fix reroute for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @group_path, notice: "Post was successfully created." }
+        format.html { redirect_to group_path(@group), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
